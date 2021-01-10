b4aee1689643ec64e3f6bc06cc117d0f6d237ed2
LEEHAYEON-korea <77229725+LEEHAYEON-korea@users.noreply.github.com>
2021-01-10 20:22:46 +0900

    01110
    Co-Authored-By: LEEHAYEON-korea <77229725+leehayeon-korea@users.noreply.github.com>

---
+++ assets/js/school.script.js
@@ -0,0 +1,45 @@
+'use strict';
+
+function bt2019(){
+            container02.style.display="";
+            container03.style.display="none";
+};
+function bt2018(){
+            container02.style.display="none";
+            container03.style.display="";
+};
+
+$(document).ready(function(){
+		$('.button').each(function(index){
+			$(this).attr('button-index', index);
+		}).click(function(){
+			var index = $(this).attr('button-index');
+			$('.button[button-index=' + index + ']').addClass('clicked_button');
+			$('.button[button-index!=' + index + ']').removeClass('clicked_button');
+		});
+
+
+	});
+
+
+$(document).ready(function() {
+
+// ライトボックス
+  lightbox.option({
+    'albumLabel': "%1 / %2",
+    'disableScrolling': true,
+  });
+
+  // テキストの表示・非表示
+  $('.js-more').on('click', function() {
+    $(this).next().toggleClass('hidden');
+  });
+    
+});
+
+
+$(document).ready(function(){
+
+    document.getElementById("roulette").innerHTML='<object type="text/html" class="roulette" data="images/school/2019/roulette/index.html"></object>';
+
+})
---
+++ assets/js/script.js
@@ -0,0 +1,48 @@
+'use strict';
+
+function bt2020(){
+            container01.style.display="";
+            container02.style.display="none";
+            container03.style.display="none";
+};
+function bt2019(){
+            container01.style.display="none";
+            container02.style.display="";
+            container03.style.display="none";
+};
+function bt2018(){
+            container01.style.display="none";
+            container02.style.display="none";
+            container03.style.display="";
+};
+
+
+
+$(document).ready(function(){
+		$('.button').each(function(index){
+			$(this).attr('button-index', index);
+		}).click(function(){
+			var index = $(this).attr('button-index');
+			$('.button[button-index=' + index + ']').addClass('clicked_button');
+			$('.button[button-index!=' + index + ']').removeClass('clicked_button');
+		});
+
+
+	});
+
+
+$(document).ready(function() {
+
+// ライトボックス
+  lightbox.option({
+    'albumLabel': "%1 / %2",
+    'disableScrolling': true,
+  });
+
+  // テキストの表示・非表示
+  $('.js-more').on('click', function() {
+    $(this).next().toggleClass('hidden');
+  });
+    
+});
+
